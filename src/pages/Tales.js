db97019e251cd7a6643494bab678b57df9d57ca4
yash21368 <108334277+yash21368@users.noreply.github.com>
2023-04-17 22:18:20 +0530

    Add files via upload

--- src/pages/Tales.js
+++ src/pages/Tales.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import bg from '../assets/landing/landingbg.png';
+import '../styles/Tales.css';
 
-function Tales() {
+
+function Community() {
   return (
-    <div>Tales</div>
+    <section id='tales'>
+    <div className="t_container">
+        <div className="circle"></div>
+        <div className="tales_logo">
+            <div className="logo_tales">
+            </div>
+            <div className="mand_container">
+                <div className='rotating_mandala'>
+
+                </div>
+            </div>
+            
+        </div>
+        <div className="tales_desc">
+            
+            <p id="cd">A digital archive of folktales, ancient lores, and legends that have been passed down through generations. The tales reflect the rich cultural heritage of India and cover a wide range of themes, including mythology, religion, history, and social customs. The website features stories of gods and goddesses, heroic feats of warriors, magical creatures, and fables with moral lessons.</p>
+
+        </div>
+        <div className="home_bg">
+        </div>
+    </div>
+
+    <div>
+        
+    </div>
+
+    </section>
+    
   )
 }
 
-export default Tales;
+export default Community;
